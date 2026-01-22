75d7b29e0ccefc7559fe1ae1d33c6a2acb232cce
Nighmare98 <61187443+Nighmare98@users.noreply.github.com>
2026-01-22 09:30:08 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите строку: ");
+        string input = Console.ReadLine();
+
+        int vowelCount = CountRussianVowels(input);
+
+        Console.WriteLine($"Количество гласных букв: {vowelCount}");
+    }
+
+    static int CountRussianVowels(string text)
+    {
+        if (string.IsNullOrEmpty(text))
+            return 0;
+
+        char[] vowels = { 'а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я' };
+
+        int count = 0;
+
+        foreach (char c in text)
+        {
+            char LowerChar = char.ToLower(c);
+
+            if (Array.IndexOf(vowels, LowerChar) >= 0)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
